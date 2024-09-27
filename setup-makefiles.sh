a536d0238a110874a4fb6827fc2093ea2b024391
PugzAreCute <me@pugzarecute.com>
2024-09-27 23:07:04 +0530

    universal8825-common:  fixup! write headers for all devices

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "a53x"
+write_headers "a53x a33x m34x"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
